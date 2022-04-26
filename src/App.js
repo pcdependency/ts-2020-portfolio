3c507c0570ac1414a0c38e1d3dba371e1d667dad
Tyler Steele <pc.dependency@gmail.com>
2022-04-25 18:43:56 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,73 +1,77 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import img1 from "./img/1.jpg";
-import img2 from "./img/2.jpg";
-import img3 from "./img/3.jpg";
-import img4 from "./img/4.jpg";
-import img5 from "./img/5.jpg";
-import img6 from "./img/6.jpg";
-import img7 from "./img/7.jpg";
-import img8 from "./img/8.jpg";
-import img9 from "./img/9.jpg";
-import img10 from "./img/10.jpg";
-import img11 from "./img/11.jpg";
-import img12 from "./img/12.png";
-import img13 from "./img/13.jpg";
-import img14 from "./img/14.jpg";
-import img15 from "./img/15.png";
-import img16 from "./img/16.jpg";
-import img17 from "./img/17.jpg";
-import img18 from "./img/18.jpg";
-import img19 from "./img/19.jpg";
-import img20 from "./img/20.jpg";
-import img21 from "./img/21.jpg";
-import img22 from "./img/22.jpg";
-import img23 from "./img/23.jpg";
-import img24 from "./img/24.jpg";
-import img25 from "./img/25.jpg";
-import img26 from "./img/26.gif";
+import img1 from "./img/originals/1.jpg";
+import img2 from "./img/originals/2.jpg";
+import img3 from "./img/originals/3.jpg";
+import img4 from "./img/originals/4.jpg";
+import img5 from "./img/originals/5.jpg";
+import img6 from "./img/originals/6.jpg";
+import img7 from "./img/originals/7.jpg";
+import img8 from "./img/originals/8.jpg";
+import img9 from "./img/originals/9.jpg";
+import img10 from "./img/originals/10.jpg";
+import img11 from "./img/originals/11.jpg";
+import img12 from "./img/originals/12.png";
+import img13 from "./img/originals/13.jpg";
+import img14 from "./img/originals/14.jpg";
+import img15 from "./img/originals/15.png";
+import img16 from "./img/originals/16.jpg";
+import img17 from "./img/originals/17.jpg";
+import img18 from "./img/originals/18.jpg";
+import img19 from "./img/originals/19.jpg";
+import img20 from "./img/originals/20.jpg";
+import img21 from "./img/originals/21.jpg";
+import img22 from "./img/originals/22.jpg";
+import img23 from "./img/originals/23.jpg";
+import img24 from "./img/originals/24.jpg";
+import img25 from "./img/originals/25.jpg";
+import img26 from "./img/originals/26.gif";
 import BackIcon from "./img/back.svg";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const tabs = [
-  { name: "Websites", imgs: [{ img: img26, sfw: true }] },
+  {
+    name: "Websites",
+    imgs: [{ img: img26, sfw: true, width: 1920, height: 983 }],
+  },
   {
     name: "Banners",
     imgs: [
-      { img: img15, sfw: true },
-      { img: img4, sfw: false },
-      { img: img5, sfw: true },
-      { img: img6, sfw: false },
-      { img: img7, sfw: true },
-      { img: img8, sfw: true },
-      { img: img10, sfw: true },
-      { img: img12, sfw: true },
-      { img: img13, sfw: true },
-      { img: img14, sfw: true },
-      { img: img21, sfw: true },
-      { img: img16, sfw: true },
-      { img: img17, sfw: true },
-      { img: img19, sfw: true },
-      { img: img9, sfw: true },
-      { img: img20, sfw: false },
-      { img: img22, sfw: true },
+      { img: img15, sfw: true, width: 1000, height: 462 },
+      { img: img4, sfw: false, width: 3840, height: 1215 },
+      { img: img5, sfw: true, width: 2120, height: 347 },
+      { img: img6, sfw: false, width: 3840, height: 634 },
+      { img: img7, sfw: true, width: 3860, height: 639 },
+      { img: img8, sfw: true, width: 2120, height: 1023 },
+      { img: img10, sfw: true, width: 2120, height: 1023 },
+      { img: img12, sfw: true, width: 4960, height: 1040 },
+      { img: img13, sfw: true, width: 3000, height: 1040 },
+      { img: img14, sfw: true, width: 6000, height: 2000 },
+      { img: img21, sfw: true, width: 3840, height: 1326 },
+      { img: img16, sfw: true, width: 3000, height: 1000 },
+      { img: img17, sfw: true, width: 6000, height: 2000 },
+      { img: img19, sfw: true, width: 3840, height: 633 },
+      { img: img9, sfw: true, width: 3840, height: 633 },
+      { img: img20, sfw: false, width: 6000, height: 2000 },
+      { img: img22, sfw: true, width: 1920, height: 640 },
     ],
   },
   {
     name: "Profile Pictures",
     imgs: [
-      { img: img23, sfw: true },
-      { img: img18, sfw: true },
-      { img: img11, sfw: true },
-      { img: img24, sfw: true },
-      { img: img25, sfw: true },
+      { img: img23, sfw: true, width: 4000, height: 4000 },
+      { img: img18, sfw: true, width: 2086, height: 2000 },
+      { img: img11, sfw: true, width: 4000, height: 4000 },
+      { img: img24, sfw: true, width: 1920, height: 1792 },
+      { img: img25, sfw: true, width: 1920, height: 1920 },
     ],
   },
   {
     name: "Architectural Designs",
     imgs: [
-      { img: img3, sfw: true },
-      { img: img1, sfw: true },
-      { img: img2, sfw: true },
+      { img: img3, sfw: true, width: 1920, height: 1080 },
+      { img: img1, sfw: true, width: 1920, height: 1080 },
+      { img: img2, sfw: true, width: 1920, height: 1080 },
     ],
   },
 ];
@@ -103,13 +107,14 @@ function useWindowDimensions() {
   return windowDimensions;
 }
 
-function App() {
+function App({ scrollPosition }) {
   const { width } = useWindowDimensions();
   const gt800 = width > 800;
   const [hover, setHover] = useState(initialState);
   const [page, setPage] = useState(-1);
   const [sfw, setSfw] = useState(false);
   const [img, setImg] = useState(-1);
+  const [images, setImages] = useState([]);
 
   const filterStyle = {
     zIndex: 1,
@@ -118,6 +123,20 @@ function App() {
     width: "100%",
     transition: "ease-in-out 0.25s",
   };
+
+  function next(pageSwitch) {
+    !pageSwitch
+      ? setImages([
+          ...images,
+          ...tabs[page].imgs.slice(images.length, images.length + 6),
+        ])
+      : setImages([...tabs[page].imgs.slice(0, 6)]);
+  }
+
+  useEffect(() => {
+    console.log(page);
+    page !== -1 && next(true);
+  }, [page]);
 
   return (
     <div className="appContainer">
@@ -279,22 +298,28 @@ function App() {
           })}
         </div>
       ) : (
-        <div className="container page">
-          {tabs[page].imgs.length > 0 &&
-            tabs[page].imgs.map((i, idx) => {
-              if (!sfw || (i.sfw && sfw)) {
-                return (
-                  <img
-                    onClick={() => setImg(idx)}
-                    key={idx}
-                    src={i.img}
-                    alt=""
-                  />
-                );
-              } else {
-                return null;
-              }
-            })}
+        <div id="pageContainer">
+          <InfiniteScroll
+            dataLength={images.length}
+            next={() => next(false)}
+            hasMore={images.length < tabs[page].imgs.length}
+            loader={<h4>Loading...</h4>}
+            scrollableTarget="pageContainer"
+          >
+            {tabs[page].imgs.length > 0 &&
+              images.map((i, idx) => {
+                if (!sfw || (i.sfw && sfw)) {
+                  return (
+                    <img
+                      onClick={() => setImg(idx)}
+                      key={idx}
+                      src={i.img}
+                      alt=""
+                    />
+                  );
+                }
+              })}
+          </InfiniteScroll>
         </div>
       )}
       {page !== -1 && img !== -1 && (
